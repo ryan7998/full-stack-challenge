0a271666c6b7dcb82e23bc2d88cc5bc4b90f80fd
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2024-10-27 14:09:09 -0400

    add isAdmin()

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@wisejobs.com',
+            'password' => Hash::make('password123'), // Replace with a strong password
+            'is_admin' => true,
+        ]);
+    }
+}
